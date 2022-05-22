d7955735f89da11cd26931d7b22534be6cc2c7e8
Shivam Patel <shivampatel066@gmail.com>
2022-05-22 14:58:16 +0530

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Shivam Patel on 22/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+}
+
